5263c93a52d2a00aca6860f550319343c97c7032
Jian Guo <withgj@gmail.com>
2018-08-26 14:16:10 -0500

    Initial Commit

---
+++ SMUExampleOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMUExampleOne
+//
+//  Created by Jian Guo on 8/26/18.
+//  Copyright © 2018 Jian Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMUExampleOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMUExampleOne
+//
+//  Created by Jian Guo on 8/26/18.
+//  Copyright © 2018 Jian Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
